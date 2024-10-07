b2439c418935c7c64a957ffbf353645e1c68a612
Augustin d'Oultremont <augustin.doultremontao@gmail.com>
2024-10-07 22:43:43 +0200

    waybar config

--- modules/desktop-environment/hyprland/default.nix
+++ modules/desktop-environment/hyprland/default.nix
@@ -10,15 +10,42 @@ in {
     config = lib.mkIf cfg.enable {
         programs.hyprland.enable = true;
 
-        home-manager.users.${userCfg.username}.wayland.windowManager.hyprland = {
-            enable = true;
-            extraConfig = builtins.readFile ./hyprland.conf;
+        home-manager.users.${userCfg.username} = {
+            wayland.windowManager.hyprland = {
+                enable = true;
+                extraConfig = builtins.readFile ./hyprland.conf;
+            };
+
+            programs.waybar = {
+                enable = true;
+                settings.mainBar = lib.importJSON ./waybar/config.jsonc;
+                style = builtins.readFile ./waybar/style.css;
+            };
+
+            # fonts for icons
+            fonts.fontconfig.enable = true;
+            home.packages = with pkgs; [
+                fira-code
+                fira-code-symbols
+                font-awesome
+                liberation_ttf
+                mplus-outline-fonts.githubRelease
+                nerdfonts
+                noto-fonts
+                noto-fonts-emoji
+                proggyfonts
+            ];
         };
 
         environment.systemPackages = with pkgs; [
-            kitty
-            wofi
-            kdePackages.dolphin
+            kitty # terminal
+            wofi # app launcher
+            kdePackages.dolphin # file explorer
+            waybar # menu bar
+
+            # screensharing 
+            pipewire
+            wireplumber
         ];
     };
 }
