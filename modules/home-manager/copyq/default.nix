ecf58b64c79afd8b37cfde3ab40b42fe69d2de09
Augustin d'Oultremont <augustin.doultremontao@gmail.com>
2024-12-29 17:00:23 +0100

    fix waybar not starting

--- modules/home-manager/copyq/default.nix
+++ modules/home-manager/copyq/default.nix
@@ -1,10 +1,13 @@
-{ lib, config, ... }: {
+{ lib, config, pkgs, ... }: {
     options.copyq.enable = lib.mkEnableOption "Enable CopyQ clipboard history";
    
     config = lib.mkIf config.copyq.enable {
-        services.copyq = {
-            enable = true;
-        };
+        # this seems to break waybar ??
+        # services.copyq.enable = true;
+
+        home.packages = with pkgs; [
+            copyq
+        ];
 
         home.file.".config/copyq/copyq.conf".source = config.lib.file.mkOutOfStoreSymlink 
             "${config.home.homeDirectory}/.config/nixos-config/modules/home-manager/copyq/copyq.conf";
