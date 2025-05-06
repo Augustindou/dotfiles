b597c943d6b574540e08ddb4d5b2e583c07615bd
Augustindou <augustin.doultremontao@gmail.com>
2025-05-06 18:21:02 +0200

    Fix starship

--- apps/terminal/starship/install.sh
+++ apps/terminal/starship/install.sh
@@ -1,4 +1,18 @@
-install snap:starship brew:starship
+case "$INSTALLER_OS" in
+    Linux)
+        curl -sS https://starship.rs/install.sh | sh
+        success "Installed starship"
+        ;;
+
+    Mac)
+        install brew:starship
+        ;;
+
+    *)
+        echo "Unknown OS"; 
+        exit 1;
+        ;;
+esac
 
 INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/starship.toml
 INSTALLER_CONFIG_DESTINATION=$HOME/.config/starship.toml
