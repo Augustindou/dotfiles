098db85df3c52e890c2f7ab215d634962d4c6bea
Augustindou <augustin.doultremontao@gmail.com>
2025-05-08 21:03:59 +0200

    remove rip2

--- apps/terminal/zsh-and-utils/install.sh
+++ apps/terminal/zsh-and-utils/install.sh
@@ -8,7 +8,7 @@ install apt:zoxide brew:zoxide;
 install apt:eza brew:eza;
 install apt:bat brew:bat;
 install apt:fzf brew:fzf;
-install brew:rip2 cargo:rip2;
+# install brew:rip2 cargo:rip2;
 install apt:ripgrep brew:ripgrep;
 
 INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/zshrc
