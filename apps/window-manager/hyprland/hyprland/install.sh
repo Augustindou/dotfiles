d29be14d6e216eb085f3aa4588b075cf798f51f0
Augustindou <augustin.doultremontao@gmail.com>
2025-05-08 21:19:13 +0200

    Fix hyprland

--- apps/window-manager/hyprland/hyprland/install.sh
+++ apps/window-manager/hyprland/hyprland/install.sh
@@ -1,5 +1,16 @@
 if [ "$INSTALLER_OS" = Linux ]; then 
-    install apt:hyprland
+    # Hyprland on apt is outdated
+    # install apt:hyprland
+
+    CURRENT_PWD=$PWD
+
+    git clone -b 24.04 --depth 1 https://github.com/JaKooLit/Ubuntu-Hyprland.git ~/.config/Ubuntu-Hyprland-24.04
+
+    cd ~/Ubuntu-Hyprland-24.04
+    chmod +x install.sh
+    ./install.sh
+
+    cd $CURRENT_PWD
 
     INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/hyprland.conf
     INSTALLER_CONFIG_DESTINATION=$HOME/.config/hypr/hyprland.conf
