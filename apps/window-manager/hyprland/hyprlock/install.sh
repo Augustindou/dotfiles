f896293a6b7e060a25abac2d030f9707ea739bde
Augustindou <augustin.doultremontao@gmail.com>
2025-05-11 10:08:01 +0200

    Add nix-env package manager

--- apps/window-manager/hyprland/hyprlock/install.sh
+++ apps/window-manager/hyprland/hyprlock/install.sh
@@ -1,10 +1,5 @@
 if [ "$INSTALLER_OS" = Linux ]; then 
-    git clone https://github.com/hyprwm/hyprlock.git /usr/local/hyprlock
-    cd /usr/local/hyprlock
-
-    cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -S . -B ./build
-    cmake --build ./build --config Release --target hyprlock -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
-    sudo cmake --install build
+    install nix:hyprlock
 
     INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/hyprlock.conf
     INSTALLER_CONFIG_DESTINATION=$HOME/.config/hypr/hyprlock.conf
