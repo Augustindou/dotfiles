52f791581d64bf6efa06e314e1d63c6d1628493e
Augustindou <augustin.doultremontao@gmail.com>
2025-05-11 10:28:55 +0200

    Add hyprland desktop

--- apps/window-manager/hyprland/hyprland/install.sh
+++ apps/window-manager/hyprland/hyprland/install.sh
@@ -1,8 +1,15 @@
 if [ "$INSTALLER_OS" = Linux ]; then 
     install nix:hyprland
 
+    # install hyprland dotfiles
     INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/hyprland.conf
     INSTALLER_CONFIG_DESTINATION=$HOME/.config/hypr/hyprland.conf
+    
+    configure
+
+    # add hyprland desktop entry
+    INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/hyprland.desktop
+    INSTALLER_CONFIG_DESTINATION=/usr/share/wayland-sessions/hyprland.desktop
     
     configure
 fi
