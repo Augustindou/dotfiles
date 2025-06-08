69fa8d03c82f0167257c7447bc176b857086a816
Augustindou <augustin.doultremontao@gmail.com>
2025-06-08 16:00:07 +0200

    sway

--- apps/window-manager/sway/swaync/install.sh
+++ apps/window-manager/sway/swaync/install.sh
@@ -1,5 +1,5 @@
 if [ "$INSTALLER_OS" = Linux ]; then 
-    install nix:swaynotificationcenter
+    install apt:sway-notification-center
 
     INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/config
     INSTALLER_CONFIG_DESTINATION=$HOME/.config/swaync
