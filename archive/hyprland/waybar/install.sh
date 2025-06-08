b995f22add63f3b5602dd03af4332e2ad82cd41d
Augustindou <augustin.doultremontao@gmail.com>
2025-06-08 13:21:58 +0200

    sway

--- archive/hyprland/waybar/install.sh
+++
@@ -1,8 +0,0 @@
-if [ "$INSTALLER_OS" = Linux ]; then 
-    install nix:waybar
-
-    INSTALLER_CONFIG_SOURCE=$INSTALLER_DIRECTORY/config
-    INSTALLER_CONFIG_DESTINATION=$HOME/.config/waybar
-    
-    configure
-fi
